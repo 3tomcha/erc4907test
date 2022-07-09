1031e69fe6ccbe058eb1e464b6e3f780674e5b55
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-07-09 11:47:15 +0900

    succeeded to userof

--- frontend/src/components/ERC4907.vue
+++ frontend/src/components/ERC4907.vue
@@ -16,11 +16,25 @@ const mint = async () => {
         from: accounts[0]
     });
 }
+const setUser = async () => {
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    const tomorrowTimestamp = tomorrow.getTime();
+    await ERC4907Demo.methods.setUser(1, "0x3D896D141dC4eEe51E829CcA7003939be20c280A", tomorrowTimestamp).send({
+        from: accounts[0],
+    });
+}
+const userOf = async () => {
+    const user = await ERC4907Demo.methods.userOf(1).call();
+    console.log(user);
+}
 </script>
 
 
 <template>
     <div>
         <button @click="mint">Mintする</button>
+        <button @click="setUser">setUserする</button>
+        <button @click="userOf">Userを取得する</button>
     </div>
 </template>
